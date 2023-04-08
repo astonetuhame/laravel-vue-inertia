b303e1ce56956342fd63f50102fba3046a0463ed
astonetuhame <astonetuhame@gmail.com>
2023-04-09 01:03:08 +0300

    Add active links functionality

---
+++ resources/js/Shared/NavLink.vue
@@ -0,0 +1,23 @@
+<template>
+  <Link
+    :class="{ 'font-bold underline': active }"
+    class="text-blue-500 hover:underline"
+  >
+    <slot />
+  </Link>
+</template>
+  
+  
+  <script>
+import { Link } from "@inertiajs/vue3";
+export default {
+  components: { Link },
+  props: {
+    active: Boolean
+  }
+  
+};
+</script>
+  
+  <style>
+</style>
