5573c6083b130ad0ac256fcb747b12e4c8c74a81
astonetuhame <astonetuhame@gmail.com>
2023-04-09 01:42:56 +0300

    Global component registration

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="p-6 bg-gray-200">
     <header class="flex justify-between">
-      <div>
+      <div class="flex items-center">
         <h1 class="font-bold text-lg">My App</h1>
         <p class="text-sm ml-4">Welcome {{ $page.props.auth.user.username }}</p>
       </div>
--- resources/js/Shared/NavLink.vue
+++ resources/js/Shared/NavLink.vue
@@ -9,9 +9,7 @@
   
   
   <script>
-import { Link } from "@inertiajs/vue3";
 export default {
-  components: { Link },
   props: {
     active: Boolean
   }
