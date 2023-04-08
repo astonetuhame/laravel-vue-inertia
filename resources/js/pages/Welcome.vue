0b2bf0f1f4c168fd22db0b6d42cd5695ff48eace
astonetuhame <astonetuhame@gmail.com>
2023-04-09 00:20:20 +0300

    Add styling

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Hello {{ name }}</h1>
+  <h1 class="text-4xl font-bold">Hello {{ name }}</h1>
 
   <p>Frameworks we shall use are as below;</p>
   <ul v-for="framework of frameworks" :key="framework">
